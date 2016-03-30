0562f2dfe59acf9ad8f7101af9de23526fc54e62
Hannah Violet <hcbviolet@gmail.com>
2016-03-29 17:43:41 -0700

    Update kele_spec.rb

--- spec/kele_spec.rb
+++ spec/kele_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Kele do
   context "#get_me" do
     it "returns correct user data as a hash" do
-      @kele_client = Kele.new("hcbviolet@gmail.com", "Hmpscb77")
+      @kele_client = Kele.new("example@example.com", "password")
       expect(@kele_client.get_me['id']).to eq(2313271)
     end
   end
