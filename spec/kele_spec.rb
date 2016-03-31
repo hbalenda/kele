26036843c6f0d8579e20f554ae6ad9e6f95d9a54
hcbviolet <hcbviolet@gmail.com>
2016-03-30 17:59:47 -0700

    Update specs

--- spec/kele_spec.rb
+++ spec/kele_spec.rb
@@ -5,28 +5,35 @@ describe Kele do
     @kele_client = Kele.new(ENV['EMAIL'], ENV['PASSWORD'])
   end
 
+  context "#initialize" do
+    it "returns @auth_token as a string" do
+      expect(@kele_client.instance_variable_get(:@auth_token)).to be_a String
+    end
+  end
+
   context "#get_me" do
-    it "returns correct user data as a hash" do
-      expect(@kele_client.get_me['id']).to eq(2313271)
+    it "returns correct " do
+      response = @kele_client.get_me
+      expect(response.instance_variable_get(:@current_user)).to be_a Object
     end
   end
 
   context "#get_mentor_availability" do
-    it "returns mentor availability as an array of hashes" do
-      @mentor_availability = @kele_client.get_mentor_availability(623967)
-      expect(@mentor_availability.first['id']).to eq(15183)
+    it "returns mentor availability" do
+      response = @kele_client.get_mentor_availability(623967)
+      expect(response.instance_variable_get(:@mentor_availability)).to be_a Object
     end
   end
 
   context "roadmap module" do
-    it "#get_roadmap returns roadmap as a hash" do
-      @roadmap = @kele_client.get_roadmap(31)
-      expect(@roadmap['projects']).to eq(2)
+    it "#get_roadmap returns roadmap" do
+      response = @kele_client.get_roadmap(31)
+      expect(response.instance_variable_get(:@roadmap)).to be_a Object
     end
 
-    it "#get_checkpoint returns checkpoint as a hash" do
-      @checkpoint = @kele_client.get_checkpoint(99)
-      expect(@checkpoint["name"]).to eq("Creating with Ajax")
+    it "#get_checkpoint returns checkpoint" do
+      response = @kele_client.get_checkpoint(99)
+      expect(response.instance_variable_get(:@checkpoint)).to be_a Object
     end
   end
 end
